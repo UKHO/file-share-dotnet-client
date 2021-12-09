8cb806d8d5dd6ea0ad3ad94383056eb5367f7b4d
vijayavelr <vijayavelr@IN-MH1DSW101716>
2021-12-09 14:45:27 +0530

    set property values in get response method

--- FileShareAdminClient/Models/Response/Result.cs
+++ FileShareAdminClient/Models/Response/Result.cs
@@ -7,34 +7,21 @@ namespace UKHO.FileShareAdminClient.Models.Response
 {
     public class Result<T> : IResult<T>
     {
-        //private readonly HttpResponseMessage response;
-
-        //public Result(HttpResponseMessage response)
-        //{
-            
-        //    this.response = response;
-
-        //    IsSuccess = response.IsSuccessStatusCode;
-
-        //    StatusCode = (int)response.StatusCode;
-        //}
         public bool IsSuccess { get; set; }
 
         public int StatusCode { get; set; }
-
-        //  public T TypeProperties { get; private set; }
 
         public List<Error> Errors { get; set; } = new List<Error>();
 
         public async Task<T> GetResponse(HttpResponseMessage response)
         {
-            //T data = default(T);
+            var result = await response.ReadAsTypeAsync<T>();
 
             IsSuccess = response.IsSuccessStatusCode;
 
             StatusCode = (int)response.StatusCode;
 
-            return await response.ReadAsTypeAsync<T>();
+            return result;
         }
     }
 }
