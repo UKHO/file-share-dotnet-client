35dffc13587e30a3cc550192faa83b078512ab87
HugoBurgess <hugo.burgess@ukho.gov.uk>
2024-10-09 14:47:28 +0100

    Add new test project

---
+++ Tests/IntegrationTests/FileShareClientIntegrationTests/UnitTest1.cs
@@ -0,0 +1,19 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+namespace FileShareClientIntegrationTests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
