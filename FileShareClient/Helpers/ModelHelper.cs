5db1718fab2c576262be2aa29e357709bdf5e2a3
HugoBurgess <hugo.burgess@ukho.gov.uk>
2024-10-24 09:52:33 +0100

    Add json helper

---
+++ FileShareClient/Helpers/ModelHelper.cs
@@ -0,0 +1,22 @@
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace UKHO.FileShareClient.Helpers
+{
+    public static class ModelHelper
+    {
+        public static JsonSerializerOptions JsonSerializerOptions { get; }
+
+        static ModelHelper()
+        {
+            JsonSerializerOptions = new JsonSerializerOptions
+            {
+                PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+                WriteIndented = false,
+                DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingDefault
+            };
+        }
+
+        public static string ToJson<T>(T obj) => JsonSerializer.Serialize(obj, JsonSerializerOptions);
+    }
+}
